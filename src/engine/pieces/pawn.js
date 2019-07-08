9a5204f587cb83128ef0ced35fb5756419bfa4cb
JasBuc <jasmine.buckley@softwire.com>
2019-07-08 17:32:50 +0100

    Refactored pawn

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -8,38 +8,20 @@ export default class Pawn extends Piece {
 
     getAvailableMoves(board) {
         let initialSquare = board.findPiece(this);
-        let newRow = initialSquare.row
-        let newCol = initialSquare.col;
-
         let allowedSquares = [];
-        
-        //get color
-        let color = this.isWhite() ? 1: -1;
+        let rowInc = this.isWhite() ? 1: -1;
 
-        newRow += color;
-        let oneSquare = Square.at(newRow, newCol);
-        if(oneSquare.isValid(this,board)){
-            if (board.getPiece(oneSquare)===undefined){         
-                allowedSquares.push(oneSquare);
-                if(this.isInInitialPosition(initialSquare)){
-                    newRow += color;
-                    let twoSquare = Square.at(newRow, newCol);
-                    if (board.getPiece(twoSquare)===undefined){
-                        allowedSquares.push(twoSquare);
-                    }
-                }
-            }
-        }
+        const limiter = this.isInInitialPosition(initialSquare)? 2:1;
+        let forwardMoves = this.generateLineMoves(board,rowInc,0,limiter);
+        forwardMoves = forwardMoves.filter((square) => square.isEnemy(this, board) === false);
+        allowedSquares = allowedSquares.concat(forwardMoves);
 
-        let sides = [-1,1];
+        let diagonals = [-1,1];
 
-        for(let colInc of sides){
-            const sideMoves = this.generateLineMoves(board,color,colInc,1);
-            for(let move of sideMoves){
-                if(move.isEnemy(this,board)){
-                    allowedSquares.push(move);
-                }   
-            }
+        for(let colInc of diagonals){
+            let diagonalMoves = this.generateLineMoves(board,rowInc,colInc,1);
+            diagonalMoves = diagonalMoves.filter((square)=> square.isEnemy(this, board));
+            allowedSquares = allowedSquares.concat(diagonalMoves);
         }
         
         return allowedSquares;
