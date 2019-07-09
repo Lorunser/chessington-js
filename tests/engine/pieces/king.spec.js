ee2bd8d5a741cc1bf1c71049ee039a750509bdd3
Lorunser <ltray99@gmail.com>
2019-07-09 11:15:30 +0100

    Castling tests

--- tests/engine/pieces/king.spec.js
+++ tests/engine/pieces/king.spec.js
@@ -1,6 +1,7 @@
 import 'chai/register-should';
 import King from '../../../src/engine/pieces/king';
 import Pawn from '../../../src/engine/pieces/pawn';
+import Rook from '../../../src/engine/pieces/rook';
 import Board from '../../../src/engine/board';
 import Player from '../../../src/engine/player';
 import Square from '../../../src/engine/square';
@@ -75,5 +76,65 @@ describe('King', () => {
         const moves = king.getAvailableMoves(board);
 
         moves.should.not.deep.include(Square.at(5, 5));
+    });
+
+    it('has the option of castling', () => {
+        const king = new King(Player.WHITE);
+        const rook = new Rook(Player.WHITE);
+        board.setPiece(Square.at(0,3), king);
+        board.setPiece(Square.at(0,7), rook);
+
+        const moves = king.getAvailableMoves(board);
+
+        moves.should.have.length(6);
+    });
+
+    it('cannot castle when obstructed', () => {
+        const king = new King(Player.BLACK);
+        const rook = new Rook(Player.BLACK);
+        board.setPiece(Square.at(7,4), king);
+        board.setPiece(Square.at(7,7), rook);
+
+        const obstructingPiece = new Pawn(Player.BLACK);
+        board.setPiece(Square.at(7,6), obstructingPiece);
+
+        const moves = king.getAvailableMoves(board);
+
+        moves.should.have.length(6);
+    });
+
+    it('castling moves the rook as well', () =>{
+        const king = new King(Player.WHITE);
+        const rook = new Rook(Player.WHITE);
+        board.setPiece(Square.at(0,3), king);
+        board.setPiece(Square.at(0,0), rook);
+
+        king.moveTo(board, Square.at(0,1));
+
+        const rookSquare = board.findPiece(rook);
+        rookSquare.should.deep.equal(Square.at(0,2)); //equals method overriden >> ==
+    });
+
+    it('cannot castle if either piece has moved', () =>{
+        const king = new King(Player.WHITE);
+        const rook_left = new Rook(Player.WHITE);
+        const rook_right = new Rook(Player.WHITE);
+
+        board.setPiece(Square.at(0,3), king);
+        board.setPiece(Square.at(0,0), rook_left);
+        board.setPiece(Square.at(0,7), rook_right);
+
+        let moves = king.getAvailableMoves(board);
+        moves.should.have.length(7); //5 normal + 2 castlings
+
+        rook_left.moveTo(board, Square.at(0,1));
+
+        moves = king.getAvailableMoves(board);
+        moves.should.have.length(6); //5 normal + 1 castling
+
+        king.moveTo(board, Square.at(0,4));
+
+        moves = king.getAvailableMoves(board);
+        moves.should.have.length(5); //5 normal
     });
 });
